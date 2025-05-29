bef00025aee5f96d2ac341e906a987d9f24ac7f0
Rumwisai Tinotenda Chivasa <rumwisaichivasa@gmail.com>
2025-05-29 11:24:18 +0200

    Wednesday

--- src/api/api.js
+++ src/api/api.js
@@ -1,20 +1,19 @@
 import axios from "axios";
 const local = 'http://localhost:5000'
-const production = 'https://nimbo-backend-1.onrender.com/'
+const production = 'https://nimbo-backend-1.onrender.com'
 
 let api_url =''
 let mode ='pro'
 
-if (mode === "pro"){
+if (mode === 'pro'){
     api_url =production
 } else{
     api_url = local
 }
 
 const api = axios.create({
-    baseURL : `${api_url}/api`,
-    withCredentials: true
+    baseURL : `${api_url}/api`
+    withCredentials : true
 })
-
 
 export default api
